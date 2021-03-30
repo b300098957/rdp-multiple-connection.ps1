9cbbb5d47b6eee33d6be7a25fd15b4d9b80e9510
Brice <62551735+b300098957@users.noreply.github.com>
2021-03-30 19:09:40 -0400

    Update rdp-connection.ps1

--- rdp-connection.ps1
+++ rdp-connection.ps1
@@ -4,6 +4,8 @@ foreach($server in $servers){
 
     cmdkey /generic:$server /user:"Brice" /pass:"B0r34l$"
     
+    Write-Host $server
+    
     mstsc /v: $server
     
     #to bypass the security alerts or certificate errors that has to be done manually
